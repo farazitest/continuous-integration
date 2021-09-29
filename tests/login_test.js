590153e9b70a70e357582fe370893d857086a3ca
Farazi <55123538+farazitest@users.noreply.github.com>
2021-09-29 08:42:57 +0600

    Update login_test.js

--- tests/login_test.js
+++ tests/login_test.js
@@ -1,8 +1,8 @@
 Feature('login');
 
 Scenario('test something', ({ I }) => {
-    I.amOnPage('/my-account');
-    I.fillField('//input[@id="username"]', 'customer');
-    I.fillField('//input[@id="password"]', secret('123456'));
-    I.click('Login');
+    I.amOnPage('https://github.com/');
+//     I.fillField('//input[@id="username"]', 'customer');
+//     I.fillField('//input[@id="password"]', secret('123456'));
+//     I.click('Login');
 });
